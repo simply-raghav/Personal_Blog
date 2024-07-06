43b03986946458905c0775f59cc68f1966fadf31
Raghav Agrawal <raghavagrawal01001@gmail.com>
2024-07-06 16:55:18 +0530

    Blog Website Base Completed

---
+++ Blogging_Website/server/config/db.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.URL);
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
